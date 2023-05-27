81902ad880777e40fe7f92f94155e119be3f021a
diego villarroel <villarroeldiego@msn.com>
2023-05-27 20:47:47 -0300

    ultima actualizacion

---
+++ clientes/index.php
@@ -0,0 +1,14 @@
+<?php
+$clientes=[
+    ['nombre'=>'Lisbet SH','img'=>'cli1.jpg','descrip'=>'Amoblamientos para exterior'],
+    ['nombre'=>'Car Two','img'=>'cli2.jpg','descrip'=>'Consecionario multi marca'],
+    ['nombre'=>'Sorrento','img'=>'cli3.jpg','descrip'=>'Mobiliario a medida'],
+    ['nombre'=>'FerroBaires','img'=>'cli4.jpg','descrip'=>'Ferreteria Industrial'],
+    ['nombre'=>'TPiso','img'=>'cli5.jpg','descrip'=>'Pisos de madera'],
+    ['nombre'=>'Clip','img'=>'cli6.jpg','descrip'=>'Libreria Comercial'],
+    ['nombre'=>'Relojes','img'=>'cli7.jpg','descrip'=>'La hora excacta en su muñeca.'],
+    ['nombre'=>'PretPorter','img'=>'cli8.jpg','descrip'=>'Lo ultimo en vestimenta'],
+    ['nombre'=>'Nutricion on-line','img'=>'cli9.jpg','descrip'=>'Todas las novedades en nutricion']
+    ];
+echo json_encode($clientes);
+?>
